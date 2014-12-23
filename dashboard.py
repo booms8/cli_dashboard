1a4c2821c28b5618bfb657b1ae567cdc6f594e29
booms8 <conor.booms.freeland@gmail.com>
2014-12-23 01:25:07 -0500

    Better structure, added system uptime clock

--- dashboard.py
+++ dashboard.py
@@ -5,51 +5,76 @@ import psutil
 import sys
 from progressbar import ProgressBar
 
-cpus = []
-cores = psutil.cpu_count()
+bcolors = {
+    'K' : '\033[90m',
+    'R' : '\033[91m',
+    'G' : '\033[92m',
+    'B' : '\033[94m',
+    'C' : '\033[96m',
+    'M' : '\033[95m',
+    'Y' : '\033[93m',
+    'N' : '\033[0m'}
 
-mem = ProgressBar(1000, pos = 1, width = 0.5, ansi = True, fillchar = '#')
-mem.set_bar_color('G')
-mem.set_perc_color('B')
-mem.set_msg_color('Y')
+def main():
+	cpus = []
+	cores = psutil.cpu_count()
 
-swp = ProgressBar(1000, pos = 2, width = 0.5, ansi = True, fillchar = '#')
-swp.set_bar_color('G')
-swp.set_perc_color('B')
-swp.set_msg_color('Y')
+	mem = ProgressBar(1000, pos = 1, width = 0.5, ansi = True, fillchar = '#')
+	mem.set_bar_color('G')
+	mem.set_perc_color('B')
+	mem.set_msg_color('Y')
+	
+	swp = ProgressBar(1000, pos = 2, width = 0.5, ansi = True, fillchar = '#')
+	swp.set_bar_color('G')
+	swp.set_perc_color('B')
+	swp.set_msg_color('Y')
+	
+	tot = ProgressBar(1000, pos = (4 + cores), width = 0.5, ansi = True, fillchar = '#')
+	tot.set_bar_color('G')
+	tot.set_perc_color('B')
+	tot.set_msg_color('M')
+	
+	mem.draw('RAM')
+	swp.draw('Swap')
+	
+	for i in range(cores):
+		cpus.append(ProgressBar(1000, pos = (4 + i), width = 0.5, ansi = True, fillchar = '#'))
+		cpus[i].set_bar_color('G')
+		cpus[i].set_perc_color('B')
+		cpus[i].set_msg_color('M')
+		cpus[i].draw('CPU #{}'.format(i + 1))
+	
+	tot.draw('Total CPU')
+	
+	print '\033[?25l'
+	
+	try:
+		while (True):
+			mem.change_progress(psutil.virtual_memory().percent * 10)
+			swp.change_progress(psutil.swap_memory().percent * 10)
+	
+			perc = psutil.cpu_percent(percpu = True)
+			for i in range(cores):
+				cpus[i].change_progress(perc[i] * 10)
+	
+			tot.change_progress(psutil.cpu_percent() * 10)
 
-tot = ProgressBar(1000, pos = (4 + cores), width = 0.5, ansi = True, fillchar = '#')
-tot.set_bar_color('G')
-tot.set_perc_color('B')
-tot.set_msg_color('M')
+			print ''
+			raw = psutil.boot_time()
+			ut = conv_systime(time.time() - raw)
+			print bcolors['G'] + 'System uptime: ' + bcolors['K'] + ut + bcolors['N']
+	
+			time.sleep(1)
+	
+	except KeyboardInterrupt:
+		print '\033[?25h'
+		pass
 
-mem.draw('RAM')
-swp.draw('Swap')
+def conv_systime(raw):
+	m, s = divmod(raw, 60)
+	h, m = divmod(m, 60)
+	d, h = divmod(h, 24)
+	return '{0:02d}d, {1:02d}:{2:02d}:{3:02d}'.format(int(d), int(h), int(m), int(s))
 
-for i in range(cores):
-	cpus.append(ProgressBar(1000, pos = (4 + i), width = 0.5, ansi = True, fillchar = '#'))
-	cpus[i].set_bar_color('G')
-	cpus[i].set_perc_color('B')
-	cpus[i].set_msg_color('M')
-	cpus[i].draw('CPU #{}'.format(i + 1))
-
-tot.draw('Total CPU')
-
-print '\033[?25l'
-
-try:
-	while (True):
-		mem.change_progress(psutil.virtual_memory().percent * 10)
-		swp.change_progress(psutil.swap_memory().percent * 10)
-
-		perc = psutil.cpu_percent(percpu = True)
-		for i in range(cores):
-			cpus[i].change_progress(perc[i] * 10)
-
-		tot.change_progress(psutil.cpu_percent() * 10)
-
-		time.sleep(1)
-
-except KeyboardInterrupt:
-	print '\033[?25h'
-	pass
+if __name__ == '__main__':
+	main()
